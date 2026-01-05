34ee7573a36a471cf5d70babdeaf2b8cb207e85b
GuidoFC <guidofigueroa96@gmail.com>
2026-01-05 11:54:08 +0100

    Creando los componentes para separar la logica y que sea mas facil de mantener

---
+++ src/app/dashboard/components/dashboard-side-menu/dashboard-side-menu-header/dashboard-side-menu-header.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-side-menu-header',
+  imports: [],
+  templateUrl: './dashboard-side-menu-header.html',
+  styles: ``,
+})
+export class DashboardSideMenuHeader {
+
+}
---
+++ src/app/dashboard/components/dashboard-side-menu/dashboard-side-menu-options/dashboard-side-menu-options.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-side-menu-options',
+  imports: [],
+  templateUrl: './dashboard-side-menu-options.html',
+  styles: ``,
+})
+export class DashboardSideMenuOptions {
+
+}
