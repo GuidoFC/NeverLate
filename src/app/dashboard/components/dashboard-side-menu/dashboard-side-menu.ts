fc8e0ed3765d1f12ab60e59cfe3c21567b42b25f
GuidoFC <guidofigueroa96@gmail.com>
2026-01-05 12:00:31 +0100

    dashboard-side-menu-header esta en su respectivo componente

--- src/app/dashboard/components/dashboard-side-menu/dashboard-side-menu.ts
+++ src/app/dashboard/components/dashboard-side-menu/dashboard-side-menu.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {DashboardSideMenuOptions} from './dashboard-side-menu-options/dashboard-side-menu-options';
+import {DashboardSideMenuHeader} from './dashboard-side-menu-header/dashboard-side-menu-header';
 
 @Component({
   selector: 'dashboard-side-menu',
   imports: [
-    DashboardSideMenuOptions
+    DashboardSideMenuOptions,
+    DashboardSideMenuHeader
   ],
   standalone: true,
   templateUrl: './dashboard-side-menu.html',
