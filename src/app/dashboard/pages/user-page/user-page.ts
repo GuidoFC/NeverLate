b2621ce6f15355c283f4c123f84ec40745bf3cd6
GuidoFC <guidofigueroa96@gmail.com>
2026-01-06 07:58:30 +0100

    Tengo el selector y puedo ver la cabecera de la tabla con su respectivo nombre

--- src/app/dashboard/pages/user-page/user-page.ts
+++ src/app/dashboard/pages/user-page/user-page.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
-import {MatTableModule} from '@angular/material/table';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {SelectionModel} from '@angular/cdk/collections';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 export interface TableUser {
   id: number;
@@ -42,13 +44,42 @@ const ELEMENT_DATA: TableUser[] = [
 @Component({
   selector: 'user-page',
   imports: [
-    MatTableModule
+    MatTableModule,
+    MatCheckboxModule
   ],
   standalone: true,
   templateUrl: './user-page.html',
   styles: ``,
 })
 export default class UserPage {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
+  displayedColumns: string[] = ['select', 'id', 'firstName', 'lastName', 'secondLastName', 'role', 'email'];
+  // dataSource = ELEMENT_DATA;
+  dataSource = new MatTableDataSource<TableUser>(ELEMENT_DATA);
+
+  selection = new SelectionModel<TableUser>(true, []);
+
+  /** Whether the number of selected elements matches the total number of rows. */
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  toggleAllRows() {
+    if (this.isAllSelected()) {
+      this.selection.clear();
+      return;
+    }
+
+    this.selection.select(...this.dataSource.data);
+  }
+
+  /** The label for the checkbox on the passed row */
+  checkboxLabel(row?: TableUser): string {
+    if (!row) {
+      return `${this.isAllSelected() ? 'deselect' : 'select'} all`;
+    }
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.id + 1}`;
+  }
 }
