a9736cf5971807c6c355df8c866dd6779424a545
GuidoFC <guidofigueroa96@gmail.com>
2026-01-06 19:41:12 +0100

    creamos el componentente de user-table

---
+++ src/app/dashboard/components/users-table/users-table.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users-table',
+  imports: [],
+  standalone: true,
+  templateUrl: './users-table.html',
+  styles: ``,
+})
+export class UsersTable {
+
+}
