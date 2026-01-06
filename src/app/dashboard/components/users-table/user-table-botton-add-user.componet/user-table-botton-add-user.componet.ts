9040143455d64e5d4da117939d8ec6e777965ee6
GuidoFC <guidofigueroa96@gmail.com>
2026-01-06 21:33:07 +0100

    creamos un componente para añadir nuevos usuarios, pendientes de implementar la lógica

---
+++ src/app/dashboard/components/users-table/user-table-botton-add-user.componet/user-table-botton-add-user.componet.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'user-table-botton-add-user',
+  imports: [],
+  standalone: true,
+  templateUrl: './user-table-botton-add-user.componet.html',
+  styles: ``,
+})
+export class UserTableBottonAddUserComponet {
+
+
+}
