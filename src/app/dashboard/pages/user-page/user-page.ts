50ba12fd267b4504e6040ccd250a82d542b160af
GuidoFC <guidofigueroa96@gmail.com>
2026-01-06 07:40:36 +0100

    adaptar la tabla al tipo de tabla que quiero diseñar. Siguiente paso añadir, un selector

--- src/app/dashboard/pages/user-page/user-page.ts
+++ src/app/dashboard/pages/user-page/user-page.ts
@@ -1,25 +1,43 @@
 import {Component} from '@angular/core';
 import {MatTableModule} from '@angular/material/table';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+export interface TableUser {
+  id: number;
+  firstName: string;
+  lastName: string;
+  secondLastName?: string; // opcional
+  role: string;
+  email: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+
+const ELEMENT_DATA: TableUser[] = [
+  {
+    id: 1,
+    firstName: 'Alex',
+    lastName: 'Moldovar',
+    secondLastName: 'Moldovar',
+    role: 'Admin',
+    email: 'alex@alex.com'
+  },
+  {
+    id: 2,
+    firstName: 'Guido',
+    lastName: 'Figueroa',
+    secondLastName: 'Figueroa',
+    role: 'Responsable',
+    email: 'guido@guido.com'
+  },
+  {
+    id: 3,
+    firstName: 'Danylo',
+    lastName: 'Ruso',
+    secondLastName: 'Ruso',
+    role: 'Prácticas',
+    email: 'danylo@danylo.com'
+  }
 ];
+
 
 @Component({
   selector: 'user-page',
