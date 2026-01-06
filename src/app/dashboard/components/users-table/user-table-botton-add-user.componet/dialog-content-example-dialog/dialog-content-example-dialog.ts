87a81b0451b886b678ba71fcc63c9df52b40e55f
GuidoFC <guidofigueroa96@gmail.com>
2026-01-06 22:17:28 +0100

    Ya puedo abrir un dialogo, ahora lo tengo que adaptar para poder añadir a un usuario

---
+++ src/app/dashboard/components/users-table/user-table-botton-add-user.componet/dialog-content-example-dialog/dialog-content-example-dialog.ts
@@ -0,0 +1,22 @@
+import {Component, inject} from '@angular/core';
+import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+import {MatButtonModule} from '@angular/material/button';
+import {FormBuilder, ReactiveFormsModule} from '@angular/forms';
+
+@Component({
+  selector: 'app-dialog-content-example-dialog',
+  imports: [MatDialogModule, MatButtonModule, ReactiveFormsModule],
+  standalone: true,
+  templateUrl: './dialog-content-example-dialog.html',
+  styles: ``,
+})
+export class DialogContentExampleDialog {
+
+  private fb = inject(FormBuilder)
+
+  myForm = this.fb.group({
+    name: [''],
+    lastName: [''],
+    email: [''],
+  })
+}
--- src/app/dashboard/components/users-table/user-table-botton-add-user.componet/user-table-botton-add-user.componet.ts
+++ src/app/dashboard/components/users-table/user-table-botton-add-user.componet/user-table-botton-add-user.componet.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
+
+import {Component, inject} from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
+import {DialogContentExampleDialog} from './dialog-content-example-dialog/dialog-content-example-dialog';
 
 @Component({
   selector: 'user-table-botton-add-user',
@@ -8,6 +12,16 @@ import { Component } from '@angular/core';
   styles: ``,
 })
 export class UserTableBottonAddUserComponet {
+
+  readonly dialog = inject(MatDialog);
+
+  openDialog() {
+    const dialogRef = this.dialog.open(DialogContentExampleDialog);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
 
 
 }
