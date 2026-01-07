f7708e1efe87f3f9612e37f383171e9a3fe79f45
GuidoFC <guidofigueroa96@gmail.com>
2026-01-07 11:44:36 +0100

    El siguiente paso es mover el estilo del boton, para poder reutilizarlo en otras paginas

---
+++ src/app/shared/ui/action-button/action-button.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'action-button',
+  imports: [],
+  standalone: true,
+  templateUrl: './action-button.html',
+  styles: ``,
+})
+export class ActionButton {
+
+}
