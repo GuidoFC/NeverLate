5b6adba82f80789cad24e616898d7144c7d20876
GuidoFC <guidofigueroa96@gmail.com>
2026-01-10 10:24:46 +0100

    Creo un componente para poder enseñar los errores del formulario antes de guardar los campos

---
+++ src/app/shared/form/form-error/form-error.ts
@@ -0,0 +1,42 @@
+import {Component, input} from '@angular/core';
+import {FormGroup} from '@angular/forms';
+
+@Component({
+  selector: 'form-error',
+  imports: [],
+  standalone: true,
+  templateUrl: './form-error.html',
+  styles: ``,
+})
+export class FormError {
+
+  form = input.required<FormGroup>()
+  formControlName = input.required<string>()
+  // myFormRecived = input
+
+  // mirar si hay un error
+  isValidField(fielName: string): boolean | null {
+    return (this.form().controls[fielName].errors &&
+      this.form().controls[fielName].touched)
+  }
+
+  getFielError(fieldName: string): string | null {
+    if (!this.form().controls[fieldName]) return null
+
+    const errors = this.form().controls[fieldName].errors ?? {};
+
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return "Este campo es requerido"
+        case 'minlength':
+          return `Mínimo de ${errors['minlength'].requiredLength} caracteres`
+
+        case 'min':
+          return `Valos mínimo de ${errors['min'].min}`
+      }
+
+    }
+    return null
+  }
+}
