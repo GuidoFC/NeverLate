275bd069d97911d9643ed32038c9fda4a6585883
GuidoFC <guidofigueroa96@gmail.com>
2026-01-13 19:36:28 +0100

    la pagina es lo unico que se comunica con el servicio

--- src/app/dashboard/pages/user-page/user-page.ts
+++ src/app/dashboard/pages/user-page/user-page.ts
@@ -1,21 +1,16 @@
-import {Component, inject, output} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {UserService} from '../../service/user.service';
 import {user_worker} from '../../interface/interface-tableUser';
-import {ActionButton} from '../../../shared/ui/action-button/action-button';
-import {RouterLink} from '@angular/router';
-import {MatDialog} from '@angular/material/dialog';
 import {SelectionModel} from '@angular/cdk/collections';
 import {UsersTable} from '../../../shared/table/table-user/users-table';
-import {AddUserDialog} from '../../components/dialogs/add-user-dialog/add-user-dialog';
-import {DisableUser} from '../../components/dialogs/disable-user-dialog/disable-user.component';
+import {UsersToolbar} from '../../components/users-toolbar/users-toolbar';
 
 
 @Component({
   selector: 'user-page',
   imports: [
-    ActionButton,
-    RouterLink,
-    UsersTable
+    UsersTable,
+    UsersToolbar
   ],
   standalone: true,
   templateUrl: './user-page.html',
@@ -24,10 +19,7 @@ import {DisableUser} from '../../components/dialogs/disable-user-dialog/disable-
 export default class UserPage {
 
   private userService = inject(UserService);
-  readonly dialog = inject(MatDialog);
-  userCreated = output<user_worker>();
-  listUserDisable = output<user_worker[]>();
-  selection = new SelectionModel<user_worker>(true, []);
+  usersSelected = new SelectionModel<user_worker>(true, []);
 
 
   getAllUser(): user_worker[] {
@@ -42,50 +34,6 @@ export default class UserPage {
     this.userService.deleteUSer(newUSer);
   }
 
-  openDialogCreateUser() {
-    // Abro el componente AddUserDialog y lo muestro como un diálogo.
-    const dialogRef = this.dialog.open(AddUserDialog, {
-      width: '90vw',
-      maxWidth: '520px',
-      maxHeight: '90vh',
-      panelClass: 'responsive-dialog',
-      disableClose: true,
-    });
 
-    dialogRef.afterClosed().subscribe(user => {
-      // hacer comprobaciones antes de guardar
-      // guardar los datos en el servicio de USUARIO
-      if (user) {
-        this.userCreated.emit(user);
-      }
-    });
-  }
 
-  openDialogDisableUser() {
-    console.log("funciona")
-    const selectedUsers = this.selection.selected;
-
-    if (selectedUsers.length === 0) {
-      // Todo avisar que no se ha seleccionado ningun usuario
-      console.log("creo que se para aqui")
-      return;
-    }
-
-    const dialogRef = this.dialog.open(DisableUser, {
-      width: '90vw',
-      maxWidth: '520px',
-      maxHeight: '90vh',
-      panelClass: 'responsive-dialog',
-      disableClose: true,
-      data: selectedUsers,
-    });
-
-    dialogRef.afterClosed().subscribe(listUser => {
-      if (listUser) {
-        this.listUserDisable.emit(listUser);
-      }
-    });
-    // limpiar selección
-    this.selection.clear();
-  }
 }
