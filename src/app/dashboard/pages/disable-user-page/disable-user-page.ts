337ab801d7d7a85b96632ed1bb242703b46389ed
GuidoFC <guidofigueroa96@gmail.com>
2026-01-13 01:14:26 +0100

    creamos una pagina para mostrar los usuarios deshabilitados

---
+++ src/app/dashboard/pages/disable-user-page/disable-user-page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-disable-user-page',
+  imports: [],
+  standalone: true,
+  templateUrl: './disable-user-page.html',
+  styles: ``,
+})
+export class DisableUserPage {
+
+}
